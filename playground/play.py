a181f912c2b59df17e3292cf7cc3c035d9397495
Matteemol <105395194+matteemol@users.noreply.github.com>
2024-04-19 04:51:12 +0000

    playground test (not working)

--- playground/play.py
+++ playground/play.py
@@ -1,13 +1,22 @@
 # Importing required functions
 from flask import Flask, flash, render_template, request, abort, send_from_directory, send_file
 
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # Flask constructor
 app = Flask(__name__)
 
 # Index page
 @app.route('/')
 def index():
-	return render_template('index.html')
+	linktojarfile = os.path.join(basedir, 'plantuml-core.jar')
+	return render_template('index.html', linktofile=linktojarfile)
+
+@app.route("/tryme/", methods=['POST'])
+def try_me():
+    return render_template('ok.html')
 
 # Main Driver Function
 if __name__ == '__main__':
